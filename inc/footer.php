9404ff82cad33d7e219384f55499aa152c23282a
Alexander Ramsey <alexander@itsolvedmd.com>
2015-01-20 15:09:47 -0500

    Bootstrap-Framework
    A simple combination of HTML5 Boilerplate and Bootstrap 3 for quickly
    creating nice websites.

---
+++ inc/footer.php
@@ -0,0 +1,31 @@
+    <footer class="footer">
+        <div class="container pad">
+
+            <div>
+                <p class="pull-right"><a href="#">Back to top</a>
+                </p>
+
+
+                <p>&copy; 2015 Bootstrap-Framework.</p>
+            </div>
+        </div>
+    </footer>
+		
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+       
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'inc/header.php'; ?>
+
+<div class="container">
+
+	<div class="jumbotron">
+			<h1>Bootstrap-Framework</h1>
+			<p>This combines Bootstrap 3 and HTML5 Boilerplate into one handy template for building a nice website.</p>
+	</div>
+	
+</div>
+
+<?php include 'inc/footer.php'; ?>
